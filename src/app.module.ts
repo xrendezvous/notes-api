f1f400f30cb4261daaba7410a45b461c8e4f7ea3
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2025-04-13 17:37:47 +0300

    updated files (adding TypeORM)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotesModule } from './notes/notes.module';
+import { Note } from './notes/entities/note.entity';
 
 @Module({
-  imports: [NotesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'xrendezvous',
+      password: 'Xzvv9843',
+      database: 'notes-editor-db',
+      entities: [Note],
+      synchronize: true,
+    }),
+    NotesModule,
+  ],
 })
 export class AppModule {}
--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,5 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Note {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   title: string;
+
+  @Column({ default: '' })
   content: string;
 }
