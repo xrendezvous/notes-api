6118c0aa605d5715b1a66541e66085e2e414ea9a
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2025-04-24 00:25:24 +0300

    fixed app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotesModule } from './notes/notes.module';
 import { Note } from './notes/entities/note.entity';
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'xrendezvous',
-      password: 'Xzvv9843',
-      database: 'notes-editor-db',
-      entities: [Note],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(
+      isGithubActions
+        ? {
+            type: 'sqlite',
+            database: ':memory:',
+            dropSchema: true,
+            entities: [Note],
+            synchronize: true,
+          }
+        : {
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'xrendezvous',
+            password: 'Xzvv9843',
+            database: 'notes-editor-db',
+            entities: [Note],
+            synchronize: true,
+          },
+    ),
     NotesModule,
   ],
 })
