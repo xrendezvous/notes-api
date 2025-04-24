31c0975afbed818c3cafc60015178a6e69fb4cd3
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2025-04-24 15:08:51 +0300

    changed main.ts for listening random ports

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
-  await app.listen(3000);
-  console.log(`Application is running on: http://localhost:3000`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
